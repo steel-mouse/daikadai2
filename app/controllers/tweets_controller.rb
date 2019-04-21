2bdc07873b32da3b2c380cc6c6a959d5208963b7
野瀬明信 <consultation.pharmacist7@gmail.com>
2019-04-21 15:45:22 +0900

    destroyアクションが別アクションの中に書かれていたので修正。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+
   def index
     @tweets = Tweet.all
   end
@@ -33,11 +35,18 @@ class TweetsController < ApplicationController
    else
      render 'edit'
    end
-   
+  end
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path, notice:"呟きを削除しました！"
   end
   private
 
   def tweet_params
     params.require(:tweet).permit(:cotent)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
