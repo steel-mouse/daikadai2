08b9df7375e5c359036ca4406741dc6b18a67379
野瀬明信 <consultation.pharmacist7@gmail.com>
2019-04-21 13:15:13 +0900

    バリデーションを追加した。

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,12 @@
 class Tweet < ApplicationRecord
+
+  with_options presence: true do
+    validates :content
+  end
+
+  with_options length: { in: 1..140 } do
+    validates :content
+  end
+
+
 end
